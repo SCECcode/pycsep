4c9ab83b80912d5cb71d2a7b1eaaa5de5cd6974d
William Savran <wsavran@usc.edu>
2018-09-30 02:39:39 -0400

    allow environment variables in docker runs to be set as environment variables

--- examples/run_u3etas_docker_benchmark.py
+++ examples/run_u3etas_docker_benchmark.py
@@ -14,10 +14,12 @@ general script to execute u3etas benchmakr stored in docker images using csep2 m
 as a script
 """
 
-num_sims = [10, 25, 50, 100, 500, 1000]
-num_sims = [10, 25]
+# will contain parameters for the run
+# (num_sim, num_thread, memory)
+# params = [(50,10,50), (100,10, 50), (200,10,50), (500, 20, 100)]
+params = [(200,10,50), (500, 20, 100)]
 
-for num_sim in num_sims:
+for num_sim, num_thread, memory in params:
 
     # generate workflow environment
     config = generate_local_airflow_environment_test(experiment_name='u3etas-benchmark',
@@ -29,6 +31,15 @@ for num_sim in num_sims:
                                                      'duration': 1.0})
     # run calculation
     t0 = time.time()
-    run_u3etas_calculation(config)
+    run_u3etas_calculation(config,
+            environment = {'ETAS_MEM_GB': memory,
+                'ETAS_LAUNCHER': '/run_dir',
+                'ETAS_OUTPUT': '/run_dir/user_output',
+                'ETAS_THREADS': num_thread},
+            command = ["u3etas_launcher.sh", os.path.join('/run_dir', config['config_filename'])])
     t1 = time.time()
     print('time for executing run_u3etas_calculation: {}'.format(t1-t0))
+
+    # command for post-processing if we want to run
+    # command = ["u3etas_plot_generator.sh", os.path.join('/run_dir', config['config_filename']), 
+    #     "/run_dir/user_output/results_complete.bin"],
