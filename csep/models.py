ab6821234bbc53a93db88cf7d3c083fcc8cd6786
William Savran <wsavran@usc.edu>
2018-11-27 16:38:58 -0800

    added simulation to models

--- csep/models.py
+++ csep/models.py
@@ -1,4 +1,25 @@
 """
-This module will contain routines that represents models of
-database tables.
+Right now, general classes used in the CSEP model. Later, will be used to connect with database backend.
 """
+import csep
+
+class Simulation:
+    def __init__(self, filename=None, min_mw=None, start_time=None, name=None, sim_type=None):
+        self.filename = filename
+        self.min_mw = min_mw
+        self.start_time = start_time
+        self.name = name
+        self.sim_type = sim_type
+        self.catalogs = None
+
+        if filename is not None:
+            self.catalogs = csep.load_stochastic_event_set(type=self.sim_type,
+                                                           filename=self.filename,
+                                                           name=self.name)
+
+    def __str__(self):
+        return 'Name: {}\n\nFilename: {}\nStart Time: {}\nMin Mw: {}\nType: {}'.format(self.name,
+                                                                                       self.filename,
+                                                                                       self.start_time,
+                                                                                       self.min_mw,
+                                                                                       self.sim_type)
