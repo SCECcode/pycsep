ed3c3c519ab11008d81e0f4f979edb2f3dc753d8
William Savran <wsavran@usc.edu>
2019-05-06 14:15:17 -0700

    __init__.py now reflects catalogs able to load stochastic event sets.

--- csep/core/environment.py
+++ csep/core/environment.py
@@ -1,10 +1,8 @@
 import os
 import sys
-import uuid
 import string
 import datetime
 import logging
-import shutil
 
 
 def generate_local_airflow_environment_test(*args, **kwargs):
@@ -76,3 +74,149 @@ def generate_local_airflow_environment_test(*args, **kwargs):
 
     # push information back to airflow scheduler by returning
     return config
+
+
+class Simulation:
+
+    def __init__(self, run_id, experiment_name, experiment_dir, machine_name,
+                 execution_runtime, runtime_dir, job_script, model_dir, output_dir, config_filename,
+                 template_dir, input_files=()):
+
+        # primary index for simulation components
+        self.run_id = run_id
+
+        # human-readable name for experiment
+        self.experiment_name = experiment_name
+
+        # top-level directory holding all forecasts for that experiment
+        self.experiment_dir = experiment_dir
+
+        # name of the machine used for computations
+        self.machine_name = machine_name
+
+        # datetime when submitted to host machine
+        self.execution_runtime = execution_runtime
+
+        # directory where simulation is ran on machine name
+        self.runtime_dir = runtime_dir
+
+        # absolute path to the job run script
+        self.job_script = job_script
+
+        # directory storing the code for the model
+        self.model_dir = model_dir
+
+        # absolute path to the job configuration script
+        self.config_filename = config_filename
+
+        # directory storing template files (e.g., machine specific runtime scripts)
+        self.template_dir = template_dir
+
+        # runtime output directory
+        self.output_dir = output_dir
+
+        # iterable of input_files, this is used for archiving purposes.
+        self.input_files = input_files
+
+    @classmethod
+    def from_dict(cls, adict):
+        """
+        Creates class from dict of objects. Useful for creating this model from repository layer.
+
+        Args:
+            adict: dictionary of parameters
+
+        Returns:
+            sim (Simulation): object of type Simulation
+
+        """
+        return cls(
+            run_id=adict['run_id'],
+            experiment_name=adict['experiment_name'],
+            experiment_dir=adict['experiment_dir'],
+            machine_name=adict['machine_name'],
+            execution_runtime=adict['execution_runtime'],
+            runtime_dir=adict['runtime_dir'],
+            job_script=adict['job_script'],
+            model_dir=adict['model_dir'],
+            config_filename=adict['config_filename'],
+            template_dir=adict['template_dir'],
+            output_dir=adict['output_dir']
+        )
+
+    def to_dict(self):
+        """
+        Create dict of class arguments for serialization, preferably using json.
+
+        Returns:
+            dict: representation of class state, serializable.
+
+        """
+        return {
+            'run_id': str(self.run_id),
+            'experiment_name': self.experiment_name,
+            'experiment_dir': self.experiment_dir,
+            'machine_name': self.machine_name,
+            'execution_runtime': self.execution_runtime,
+            'runtime_dir': self.runtime_dir,
+            'job_script': self.job_script,
+            'model_dir': self.model_dir,
+            'output_dir': self.output_dir,
+            'config_filename': self.config_filename,
+            'template_dir': self.template_dir
+        }
+
+    def _create_runtime_directory(self, force=True):
+        """
+        Creates directory for the simulation.
+
+        Args:
+            force:
+
+        Returns:
+
+        Throws:
+            FileNotFoundError
+
+        """
+        raise NotImplementedError
+
+    def _copy_input_files(self):
+        """
+        Copies input files to runtime directory.
+
+        Returns:
+
+        Throws:
+            FileNotFoundError
+        """
+        raise NotImplementedError
+
+    def _write_configuration(self, updated_inputs=None):
+        """
+        writes input configuration file for individual simulation. assumes that model inputs are json serializable.
+
+        Args:
+             updated_inputs: dict of inputs to overwrite
+
+        Returns:
+            None
+
+        Throws:
+            IOError: issue reading/writing file
+            TypeError: issue with types contained in dictionary
+        """
+
+    def prepare(self):
+        """
+        Prepares simulation for submission on the compute host. This function does not submit the job on the host.
+
+        Returns:
+            None
+
+        Throws:
+            
+
+        """
+        raise NotImplementedError
+
---
+++ tests/environment/test_simulation.py
@@ -0,0 +1,79 @@
+from unittest import TestCase
+from csep.core.environment import Simulation
+import uuid
+
+
+class TestSimulation(TestCase):
+    def test_create_runtime_directory(self):
+        run_id = uuid.uuid4()
+        experiment_name = 'test-experiment'
+        experiment_dir = '/home/testuser/test_dir'
+        machine_name = 'csep-op'
+        execution_runtime = 'my-datetime'
+        runtime_dir = '/home/testuser/test_runtime_dir'
+        job_script = 'my_run_script.csh'
+        model_dir = 'model_dir'
+        config_filename = 'my_config_filename.json'
+        output_dir = 'test_output_dir'
+        template_dir = '/home/testuser/path_to_template'
+
+        sim = Simulation(run_id, experiment_name, experiment_dir, machine_name,
+                         execution_runtime, runtime_dir, job_script, model_dir, output_dir,
+                         config_filename, template_dir)
+
+        self.assertEqual(sim.run_id, run_id)
+        self.assertEqual(sim.experiment_dir, experiment_dir)
+        self.assertEqual(sim.experiment_name, experiment_name)
+        self.assertEqual(sim.machine_name, machine_name)
+        self.assertEqual(sim.execution_runtime, execution_runtime)
+        self.assertEqual(sim.runtime_dir, runtime_dir)
+        self.assertEqual(sim.job_script, job_script)
+        self.assertEqual(sim.model_dir, model_dir)
+        self.assertEqual(sim.output_dir, output_dir)
+        self.assertEqual(sim.config_filename, config_filename)
+        self.assertEqual(sim.template_dir, template_dir)
+
+    def test_create_simulation_from_dict(self):
+        adict = {
+            'run_id': uuid.uuid4(),
+            'experiment_name': 'test-experiment',
+            'experiment_dir': '/home/testuser/test_dir',
+            'machine_name': 'csep-op',
+            'execution_runtime': 'my-datetime',
+            'runtime_dir': '/home/testuser/test_runtime_dir',
+            'job_script': 'my_run_script.csh',
+            'model_dir': 'model_dir',
+            'config_filename': 'my_config_filename.json',
+            'template_dir': '/home/testuser/path_to_template'
+        }
+        sim = Simulation.from_dict(adict)
+
+        self.assertEqual(sim.run_id, adict['run_id'])
+        self.assertEqual(sim.experiment_dir, adict['experiment_dir'])
+        self.assertEqual(sim.experiment_name, adict['experiment_name'])
+        self.assertEqual(sim.machine_name, adict['machine_name'])
+        self.assertEqual(sim.execution_runtime, adict['execution_runtime'])
+        self.assertEqual(sim.runtime_dir, adict['runtime_dir'])
+        self.assertEqual(sim.job_script, adict['job_script'])
+        self.assertEqual(sim.model_dir, adict['model_dir'])
+        self.assertEqual(sim.output_dir, adict['output_dir'])
+        self.assertEqual(sim.config_filename, adict['config_filename'])
+        self.assertEqual(sim.template_dir, adict['template_dir'])
+
+    def test_to_dict_equal_from_dict(self):
+        adict = {
+            'run_id': uuid.uuid4(),
+            'experiment_name': 'test-experiment',
+            'experiment_dir': '/home/testuser/test_dir',
+            'machine_name': 'csep-op',
+            'execution_runtime': 'my-datetime',
+            'runtime_dir': '/home/testuser/test_runtime_dir',
+            'job_script': 'my_run_script.csh',
+            'model_dir': 'model_dir',
+            'output_dir': 'output_dir',
+            'config_filename': 'my_config_filename.json',
+            'template_dir': '/home/testuser/path_to_template'
+        }
+        sim = Simulation.from_dict(adict)
+
+        self.assertEqual(sim.to_dict(), Simulation.from_dict(adict).to_dict())
