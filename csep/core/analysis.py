16bae852aac5547c386e830669035877dc75f960
William Savran <wsavran@usc.edu>
2019-11-11 15:13:06 -0800

    moved docker functions to remove dependency from package

--- csep/core/analysis.py
+++ csep/core/analysis.py
@@ -33,67 +33,6 @@ from csep.utils.stats import get_quantiles, cumulative_square_diff
 from csep.core.catalogs import ComcatCatalog
 from csep.core.repositories import FileSystem
 
-def run_u3etas_calculation(**kwargs):
-    """
-    run u3etas with new user interface. 
-
-    :param **kwargs: contains the context provided by airflow
-    type: dict
-    """
-    # get configuration dict from scheduler
-    ti = kwargs.pop('ti')
-    config = ti.xcom_pull(task_ids='generate_environment')
-
-    # setup docker using easy interfact
-    host_dir = os.path.join(config['runtime_dir'], 'output_dir')
-    container_dir = '/run_dir/user_output'
-
-    client = docker.from_env()
-    container = client.containers.run('wsavran/csep:u3etas-test2',
-            volumes = {host_dir: 
-                {'bind': container_dir, 'mode': 'rw'}},
-            environment = {'ETAS_MEM_GB': '14', 
-                'ETAS_LAUNCHER': '/run_dir',
-                'ETAS_OUTPUT': '/run_dir/user_output',
-                'ETAS_THREADS': '3'},
-            detach = True,
-            stderr = True)
-
-    # stream output to stdout
-    for line in container.logs(stream=True):
-        print(line.decode('utf-8'))
-
-
-def run_u3etas_post_processing(**kwargs):
-    """
-    run post-processing for u3etas
-
-    :param **kwargs: context passed from airflow scheduler
-    type: dict
-    """
-    # get configuration dict from scheduler
-    ti = kwargs.pop('ti')
-    config = ti.xcom_pull(task_ids='generate_environment')
-
-    # setup docker using easy interfact
-    host_dir = os.path.join(config['runtime_dir'], 'output_dir')
-    container_dir = '/run_dir/user_output'
-
-    client = docker.from_env()
-    container = client.containers.run('wsavran/csep:u3etas-test2',
-            volumes = {host_dir: 
-                {'bind': container_dir, 'mode': 'rw'}},
-            environment = {'ETAS_MEM_GB': '14', 
-                'ETAS_LAUNCHER': '/run_dir',
-                'ETAS_OUTPUT': '/run_dir/user_output',
-                'ETAS_THREADS': '3'},
-            command = ["u3etas_plot_generator.sh", "/run_dir/config.json", "/run_dir/user_output/results_complete.bin"],
-            detach = True,
-            stderr = True)
-
-    # stream output to stdout
-    for line in container.logs(stream=True):
-        print(line)
 
 
 def ucerf3_consistency_testing(sim_dir, event_id, end_epoch, n_cat=None, plot_dir=None, generate_markdown=True, catalog_repo=None, save_results=False):
@@ -124,7 +63,7 @@ def ucerf3_consistency_testing(sim_dir, event_id, end_epoch, n_cat=None, plot_di
         plot_dir = os.path.join(sim_dir, 'plots')
         print(f'No plotting directory specified defaulting to {plot_dir}')
     config_file = os.path.join(sim_dir, 'config.json')
-    mkdirs(os.path.join(plot_dir, 'plots'))
+    mkdirs(os.path.join(plot_dir))
 
     # load ucerf3 configuration
     with open(os.path.join(config_file), 'r') as f:
