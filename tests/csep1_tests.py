eac052cc0197960a03cf5019cb02a55a416cbada
khawajasim <asimkhawaja786@gmail.com>
2020-02-18 18:41:38 +0100

    Adding T test and W test. The implementation of T test is my interpretation of Rhoades at al. 2011. I request someone to comment on the maths of T test, in particular, use of N or nbin. W test has also been implemented

--- tests/csep1_tests.py
+++ tests/csep1_tests.py
@@ -334,3 +334,295 @@ def conditional_l_test(observation, forecast, num_simulations = 1000, model_name
     
     Conditional_L_Test_Eval = l_test(observation, norm_forecast, num_simulations, model_name)
     return Conditional_L_Test_Eval
+
+    
+def t_test(observation, forecast_1, forecast_2, model_name_1='None', model_name_2='None'):
+    
+    """
+    Computes T test statistic by comparing Forecast 1 and Forecast 2 Catalogs
+    
+    We compare Forecasat of Model 1 and with Forecast of Model 2. Information Gain is computed, which is then employed to compute T statistic.
+    Confidence interval of Information Gain can be computed using T_critical
+    
+    Args
+    observation:Observed (Grided) seismicity (Numpy Array):
+                An Observation has to be observed seismicity in each Bin
+                It has to be a either zero or positive integer only (No Floating Point)
+    forecast_1: Forecast of a model_1 (Grided) (Numpy Array)
+                A forecast has to be in terms of Average Number of Events in Each Bin 
+                It can be anything greater than zero
+    forecast_2: Forecast of model_2 (Grided) (Numpy Array) 
+                A forecast has to be in terms of Average Number of Events in Each Bin 
+                It can be anything greater than zero
+    model_1:    The name of Model 1, with whom the comparision will be drawn
+    model_2:    The name of second model, which will be compared with Model 1
+    
+    Returns
+    A dictionary of following elements
+                 { 'model_name_1': Name of Model 1,
+                   'model_name_2': Name of Model 2,
+                   't_statistic' : T statistic computed between forecast 1 and forecast 2,
+                   't_critical': Critical value of T at 95% CDF, assuming 1-Tail distribution,
+                   'information_gain': Information gain per earthquake of Model A over Model B,
+                   'IG_lower':Lower bound pf Information Gain Confidence Interval,
+                   'IG_upper': Upper bound of Information Gain Confidence Interval}
+    """
+    
+    #Some Pre Calculations -  Because they are being used repeatedly. 
+    N = numpy.sum(observation)      #Total number of observed earthquakes
+    nbins = numpy.size(observation) #No. of Bins
+    N1 = numpy.sum(forecast_1)      #Total number of Forecasted earthquakes by Model 1
+    N2 = numpy.sum(forecast_2)      #Total number of Forecasted earthquakes by Model 2
+    X1 = numpy.log(forecast_1)      #Log of every element of Forecast 1
+    X2 = numpy.log(forecast_2)      #Log of every element of Forecast 2
+    
+    #Information Gain, using Equation (17)  of Rhoades et al. 2011
+    Information_Gain = (numpy.sum(X1 - X2) - (N1-N2)) / N   #I beleieve, N is Total no. of Earthquakes here. 
+                                
+      
+        
+    #Compute variance of (X1-X2) using Equation (18)  of Rhoades et al. 2011
+    first_term =  (numpy.sum(numpy.power((X1 - X2), 2))) / (nbins-1)    #I believe N is No. of Bins (nbins)  here?? So replacing N by bins
+    second_term =   numpy.power(numpy.sum(X1 - X2), 2) / (numpy.power(nbins,2) - nbins)   #nbins in place of N
+    forecast_variance = first_term - second_term 
+     
+    forecast_std = numpy.sqrt(forecast_variance)  
+    t_statistic = Information_Gain / (forecast_std/ numpy.sqrt(nbins))
+
+    #Obtaining the Criticial Value of T from T distribution.  
+    df = nbins-1
+    t_critical = scipy.stats.t.ppf(1-(0.05/2), df)   #Assuming 2-Tail Distribution  for 2 tail, divide 0.05/2.
+
+    #Computing Information Gain Interval. 
+    IG_lower = Information_Gain - (t_critical*forecast_std / numpy.sqrt(nbins))  #Assuming N = nbins here.
+    IG_upper = Information_Gain + (t_critical*forecast_std / numpy.sqrt(nbins))  #Assuming N = nbins here.
+    
+    #If T value greater than T critical, Then both Lower and Upper Confidence Interval limits will be greater than Zero. 
+    # If above Happens, Then It means that Forecasting Model 1 is better than Forecasting Model 2. 
+    t_test_eval = { 'model_name_1': model_name_1,
+                    'model_name_2': model_name_2,
+                    't_critical' : t_critical,
+                    't_statistic': t_statistic,
+                    'information_gain': Information_Gain,
+                    'IG_lower': IG_lower,
+                    'IG_upper': IG_upper  }    
+   
+    return t_test_eval
+
+
+def multiple_t_tests(observation, dic_forecast_1, list_dic_forecast_2):
+    
+    """ 
+    We compare Forecast of Model 1 and with all the Forecasts given in "dic_forecast_2. 
+    It considers Forecast 1 as a benchmark and computes T statistics and Informatuion Gain to see which is better. 
+    Information Gain is computed, which is then employed to compute T statistic.
+    Confidence interval of Information Gain can be computed using T_critical
+    
+    Args
+    observation:Observed (Grided) seismicity (Numpy Array):
+                An Observation has to be observed seismicity in each Bin
+                It has to be a either zero or positive integer only (No Floating Point)
+    dic_forecast_1: A dictionary of Forecast of a Model_1 consiisting of 2 elements
+                    {'model_name': Name of Model with which comparisions is to be drawn
+                     'forecast': Forecast of a Model with which comparisions is to be drawn  }
+             
+    list_forecast_2: A list of dictionaries of Forecasts, which are to be compared with forecast of Model 1.
+                     Every dictionary in the list consists of 2 elements
+                     {'model_name': Name of Model which is to be compared with Model 1
+                     'Forecast': Forecast of a Model which is to be comparisioned forecast of Model 1  }
+                
+    Returns
+    A List of dictionaries:
+    Every dictionary in the list of dictionaries consists of following elecments
+                {'model_name_1': Name of Mdoel 1 with which rest of the forecasts are compared,
+                 'model_name_2': Name of Model 2, which is compared with forecast of Model 1,
+                 't_critical': Critical value of T at 95% CDF, assuming 1-Tail distribution,
+                 'information_gain': Information gain per earthquake of Model A over Model B,
+                 'IG_lower':Lower bound pf Information Gain Confidence Interval,
+                 'IG_upper': Upper bound of Information Gain Confidence Interval }
+    """
+    
+    model_name_1 = dic_forecast_1["model_name"]
+    forecast_1 = dic_forecast_1["forecast"]
+    
+    model_name_2 = []
+    forecast_2 = []
+    
+    output_dic_list = []
+    
+    for i in range(numpy.size(list_dic_forecast_2)):
+        model_name_2 = list_dic_forecast_2[i]["model_name"]
+        forecast_2 = list_dic_forecast_2[i]["forecast"]
+        
+        out = t_test(observation, forecast_1, forecast_2, model_name_1, model_name_2)
+        output_dic_list.append(out)
+        
+    return output_dic_list
+
+
+    
+def w_test(x, m=0):
+    
+    """
+    Calculate the Single Sample Wilcoxon signed-rank test for any sample. .
+    This method is based on collecting a number of samples from a population with unknown median, m.   
+    The Wilcoxon One Sample Signed-Rank tes is the non parametric version of the one sample t test. 
+    It is based on ranks and because of that, the location parameter is not here the mean but the median.
+    This test allows to test the null hypothesis that the sample median is equal to a given value provided by the user.
+    If we designate m to be the assumed median of the sample:
+    Null hypothesis (simplified): The population from which the data were sampled is symmetric about the Given value (m).
+    Alternative hypothesis (simplified, two-sided): The population from which the data were sampled is not symmetric around m. 
+    
+    Parameters
+       
+    Args
+    x:   1D vector. For CSEP it has to be [log(forecat_1) - log(forecast_2)]
+                An Observation has to be observed seismicity in each Bin
+    m:   Designated mean value. For CSEP it to be (Sum of expectation of forecast_1 - Sum of expectation of forecast_2) / total no. of bins (I assume bins)
+    
+    Returns
+    A dictionary of following elements
+                {'z_statistic': Value of Z statistic, considering two-side test,
+                 'probablity': Probablity value }
+    """
+    
+    d = x - m
+
+    d = numpy.compress(numpy.not_equal(d, 0), d, axis=-1)
+
+    count = len(d)
+    if count < 10:
+        numpy.warnings.warn("Sample size too small for normal approximation.")
+
+    r = scipy.stats.rankdata(abs(d))
+    r_plus = numpy.sum((d > 0) * r, axis=0)
+    r_minus = numpy.sum((d < 0) * r, axis=0)
+
+    #For "two-sided", choose minimum of both
+    T = min(r_plus, r_minus)
+   
+    #Correction to be intorduced 
+    mn = count * (count + 1.) * 0.25
+    se = count * (count + 1.) * (2. * count + 1.)
+
+    replist, repnum = scipy.stats.find_repeats(r)
+    if repnum.size != 0:
+        # Correction for repeated elements.
+        se -= 0.5 * (repnum * (repnum * repnum - 1)).sum()
+
+    se = numpy.sqrt(se / 24)
+
+    # compute statistic and p-value using normal approximation
+    z = (T - mn - d) / se #Continuty correction. But We are not considering continuty correction. 
+    z = (T - mn) / se
+    
+    # 2, is multiplied for "two-sided" distribution
+    prob = 2. * scipy.stats.distributions.norm.sf(abs(z))
+
+    #Accept the NULL Hypothesis [Median(Xi-Yi) = Given value]. If probability is greater than 0.05
+    #If Probability is smaller than 0.05, Reject the NULL Hypothesis, that Median(Xi-Yi) != Given Value
+    w_test_eval = { 'z_statistic' : z,
+                    'probability': prob }    
+    return w_test_eval
+
+def w_test_csep(observation, forecast_1, forecast_2, model_1='None', model_2='None'):
+    
+    """
+    Calculate the Single Sample Wilcoxon signed-rank test for "log(forecast_1(i))-log(forecast_2(i))".
+    This test allows to test the null hypothesis that the median of Sample (X1(i)-X2(i)) is equal to a (N1-N2)/nbins.
+    where, N1, N2 = Sum of expected values of Forecast_1 and Forecast_2, respectively. 
+    {Note: I assume N=Total No of bins here (nbins)} for Rhodes et al. 2011 (Page 741).
+    The Wilcoxon signed-rank test tests the null hypothesis that difference of Xi and Yi come from the same distribution. 
+    In particular, it tests whether the distribution of the differences is symmetric around given mean.
+    
+    Parameters
+        
+    Args
+    observation:Observed (Grided) seismicity (Numpy Array):
+                An Observation has to be observed seismicity in each Bin
+                It has to be a either zero or positive integer only (No Floating Point)
+    forecast_1: Forecast of a model_1 (Grided) (Numpy Array)
+                A forecast has to be in terms of Average Number of Events in Each Bin 
+                It can be anything greater than zero
+    forecast_2: Forecast of model_2 (Grided) (Numpy Array) 
+                A forecast has to be in terms of Average Number of Events in Each Bin 
+                It can be anything greater than zero
+    model_1:    The name of Model 1, with whom the comparision will be drawn
+    model_2:    The name of second model, which will be compared with Model 1
+    
+    Returns
+    A dictionary of following elements
+                {'model_name_1': Name of Model 1,
+                 'model_name_2': Name of Model 2,
+                 'z_statistic' : Z statistic computed between forecast 1 and forecast 2,
+                 'probablity': Probablity value}
+    """
+    nbins = numpy.size(observation) #No. of Bins
+    N1 = numpy.sum(forecast_1)      #Total number of Forecasted earthquakes by Model 1
+    N2 = numpy.sum(forecast_2)      #Total number of Forecasted earthquakes by Model 2
+    X1 = numpy.log(forecast_1)      #Log of every element of Forecast 1
+    X2 = numpy.log(forecast_2)      #Log of every element of Forecast 2
+    
+    median_value = (N1-N2)/nbins
+    
+    Diff = X1 - X2
+    
+    #W_test is One Sample Wilcoxon Signed Rank Test. It accepts the data only in 1D array. 
+    x = Diff.flatten()  #Converting 2D Difference to 1D
+    
+    w_test_dic = w_test(x, median_value)
+    
+    w_test_dic['model_name_1'] =  model_1
+    w_test_dic['model_name_2'] =  model_2
+    
+    return w_test_dic
+
+
+
+def multiple_w_tests(observation, dic_forecast_1, list_dic_forecast_2):
+    
+    """ 
+    We compare Forecast of Model 1 and with all the Forecasts given in "list_dic_forecast_2" using  test. 
+    It considers Forecast 1 as a benchmark and computes w test.
+    
+    Args
+    observation:Observed (Grided) seismicity (Numpy Array):
+                An Observation has to be observed seismicity in each Bin
+                It has to be a either zero or positive integer only (No Floating Point)
+    dic_forecast_1: A dictionary of Forecast of a Model_1 consiisting of 2 elements
+                    {'model_name': Name of Model with which comparisions is to be drawn
+                     'forecast': Forecast of a Model with which comparisions is to be drawn  }
+             
+    list_forecast_2: A list of dictionaries of Forecasts, which are to be compared with forecast of Model 1.
+                     Every dictionary in the list consists of 2 elements
+                     {'model_name': Name of Model which is to be compared with Model 1
+                     'Forecast': Forecast of a Model which is to be comparisioned forecast of Model 1  }
+                
+    Returns
+    A List of dictionaries:
+    Every dictionary in the list of dictionaries consists of following elecments
+                A dictionary of following elements
+                {'model_name_1': Name of Model 1,
+                 'model_name_2': Name of Model 2,
+                 'z_statistic' : Z statistic computed between forecast 1 and forecast 2,
+                 'probablity': Probablity value}
+    """
+    
+    model_name_1 = dic_forecast_1["model_name"]
+    forecast_1 = dic_forecast_1["forecast"]
+    
+    model_name_2 = []
+    forecast_2 = []
+    
+    output_dic_list = []
+    
+    for i in range(numpy.size(list_dic_forecast_2)):
+        model_name_2 = list_dic_forecast_2[i]["model_name"]
+        forecast_2 = list_dic_forecast_2[i]["forecast"]
+        
+        out = w_test_csep(observation, forecast_1, forecast_2, model_name_1, model_name_2)
+        output_dic_list.append(out)
+        
+    return output_dic_list
+    
+    
