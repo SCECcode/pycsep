b8e38195adf39809ebdd61b183e0bb6e7521fc59
pciturri <pciturri@gfz-potsdam.de>
2020-11-23 18:07:39 +0100

    Removed unittests for spatial plot

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -5,26 +5,7 @@ import unittest
 import random
 import string
 from csep.utils import plots
-import pytest
 
-def random_region(n):
-
-    n_iter = 0
-    while n_iter < 100:
-        x_1 = numpy.random.uniform(-180, 179)
-        y_1 = numpy.random.uniform(-90, 89)
-        x_2 = numpy.random.uniform(x_1 + 0.001, 180)
-        y_2 = numpy.random.uniform(y_1 + 0.001, 90)
-        xs = numpy.linspace(x_1, x_2, n)
-        dh = xs[1] - xs[0]
-        ys = numpy.arange(y_1, y_2, dh)
-
-        bbox = [min(xs), max(xs), min(ys), max(ys)]
-        if   10 < ys.shape[0] < 1000 and max(ys) + dh < 90 \
-                and max(xs) + dh < 180:
-            break
-        n_iter += 1
-    return (xs, ys, dh, bbox)
 
 class TestPoissonPlots(unittest.TestCase):
 
@@ -74,7 +55,7 @@ class TestPoissonPlots(unittest.TestCase):
             Stest_result = mock.Mock() # Mock class with random attributes
             Stest_result.name = 'Mock STest'
             Stest_result.sim_name = ''.join(random.choice(string.ascii_letters) for _ in range(8))
-            Stest_result.test_distribution = numpy.random.uniform(-1000, 0, numpy.random.randint(500)).tolist()
+            Stest_result.test_distribution = numpy.random.uniform(-1000, 0, numpy.random.randint(3, 500)).tolist()
             Stest_result.observed_statistic = numpy.random.uniform(-1000, 0) #random observed statistic
             if numpy.random.random() <0.02: # sim possible infinite values
                 Stest_result.observed_statistic = -numpy.inf
@@ -87,34 +68,7 @@ class TestPoissonPlots(unittest.TestCase):
                         [i.sim_name for i in Stests])
         self.assertEqual(matplotlib.pyplot.gca().get_title(), Stests[0].name)
 
-class TestSpatialPlot(unittest.TestCase):
-
-    def test_SpatialDataset(self, show=False):
-        # Get random region between x in [-180,180] and y in [-90, 90]
-        xs, ys, dh, bbox = random_region(numpy.random.randint(10,50))
-        # Create mock class and methods
-        region = mock.Mock()
-        region.get_bbox = mock.Mock()
-        region.get_bbox.return_value = bbox
-        region.xs = xs
-        region.ys = ys
-        region.dh = dh
-        show_flag = bool(random.getrandbits(1))
-        # Create random dataset
-        gridded = numpy.random.random((ys.shape[0], xs.shape[0]))
-        # Call func
-        ax = plots.plot_spatial_dataset(gridded, region, show=show_flag,
-                                        plot_args={'grid': True, 'coastline':True})
-        # Assert extent of the plot
-        for i,j in zip(ax.get_xbound(), bbox[:2]):
-            self.assertAlmostEqual(i, j)
-        for i,j in zip(ax.get_ybound(), bbox[2:]):
-            self.assertAlmostEqual(i, j + dh)
-        # assert ax elements. 3 elements if plot showed, 1 if not, because cartopy does not draw unless show is called.
-        self.assertEqual(len(ax.collections), 1 + 2*show_flag)
-        self.assertEqual(ax.collections[0].get_array().shape[0], gridded.size)
-
 
 if __name__ == '__main__':
     unittest.main()
-    # pytest.main()
+
